d119e2f3b2bc63bd6706d04e4031078a69954ddd
Joshua Spence <josh@joshuaspence.com>
2012-05-01 22:34:29 +1000

    Fixed up a few missing references issues.

--- edit.sh
+++ edit.sh
@@ -4,4 +4,7 @@
 nautilus "$(readlink -f `dirname $0`)" &
 
 # Edit LaTeX files
-find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.bib" -o -name "*.sty" \) -print0 | `xargs -0 gedit` &
+find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.sty" \) -print0 | `xargs -0 gedit` &
+
+# Edit bibtex files
+find `dirname $0`/src -type f \( -name "*.bib" \) -print0 | `xargs -0 jabref` &
