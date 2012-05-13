2b317b4658f8f62dd26113b3886730e90bfc1540
Joshua Spence <josh@joshuaspence.com>
2012-05-13 12:38:09 +1000

    * Added some sources. * Improvements to edit script.

--- edit.sh
+++ edit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Open browser
-nautilus "$(readlink -f `dirname $0`)" &
+nautilus "$(readlink -f `dirname $0`)" 2>/dev/null & 
 
 # Edit LaTeX files
-find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.sty" \) -print0 | `xargs -0 gedit` &
+LATEX_FILES=$(find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.sty" \) -print0 | xargs -0 echo)
+gedit $LATEX_FILES 2>/dev/null &
 
 # Edit bibtex files
-find `dirname $0`/src -type f \( -name "*.bib" \) -print0 | `xargs -0 jabref` &
+BIBTEX_FILES=$(find `dirname $0`/src -type f \( -name "*.bib" \) -print0 | xargs -0 echo)
+jabref $BIBTEX_FILES 2>/dev/null &
