5d1803cc728628ab161a834ab2a0e75a881d756f
Joshua Spence <josh@joshuaspence.com>
2012-05-13 16:33:28 +1000

    Changed from using bibtex to biblatex.

--- edit.sh
+++ edit.sh
@@ -4,9 +4,5 @@
 nautilus "$(readlink -f `dirname $0`)/.."  2>/dev/null &
 
 # Edit LaTeX files
-LATEX_FILES=$(find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.sty" \) -print0 | xargs -0 echo)
+LATEX_FILES=$(find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.sty"  -o -name "*.bib" \) -print0 | xargs -0 echo)
 gedit $LATEX_FILES 2>/dev/null &
-
-# Edit bibtex files
-BIBTEX_FILES=$(find `dirname $0`/src -type f \( -name "*.bib" \) -print0 | xargs -0 echo)
-jabref $BIBTEX_FILES 2>/dev/null &
