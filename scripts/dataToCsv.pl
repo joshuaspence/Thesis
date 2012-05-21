4575cabf10eda22eeb1fc439317988e9658328fd
Joshua Spence <josh@joshuaspence.com>
2012-05-21 23:33:18 +1000

    Added script to convert Matlab profiler output into CSV format.

---
+++ scripts/dataToCsv.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use HTML::TableExtract;
+
+my $te = HTML::TableExtract->new();
+$te->parse(<STDIN>);
+
+foreach my $ts ($te->tables()) {
+	my $data_rows = $ts->rows();
+	my $header_row = splice(@$data_rows, 0, 1);
+
+	my $function_name_header = @$header_row[0];
+	my $calls_header = @$header_row[1];
+	my $total_time_header = @$header_row[2];
+	
+	my $self_time_header = @$header_row[3];
+	$self_time_header =~ s/\*//;
+
+	print "$function_name_header,$calls_header,$total_time_header,$self_time_header\n";
+
+    foreach my $row (@$data_rows) {
+    	my $function_name = @$row[0];
+    	$function_name =~ s/_/\\_/g;
+    	
+    	my $calls = @$row[1];
+    	
+    	my $total_time = @$row[2];
+    	$total_time =~ s/\s*s//;
+    	
+    	my $self_time = @$row[3];
+    	$self_time =~ s/\s*s//;
+    
+	    print "$function_name,$calls,$total_time,$self_time\n";
+    }
+}
