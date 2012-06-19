eeb0854b6584589edd8bee81261f091c3b8e348f
Joshua Spence <josh@joshuaspence.com>
2012-06-19 19:10:27 +1000

    Deleted unused edit script.

--- edit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Open browser
-nautilus "$(readlink -f `dirname $0`)/.."  2>/dev/null &
-
-# Edit LaTeX files
-LATEX_FILES=$(find `dirname $0`/src -type f \( -name "*.tex" -o -name "*.sty"  -o -name "*.bib" \) -print0 | xargs -0 echo)
-gedit $LATEX_FILES 2>/dev/null &
