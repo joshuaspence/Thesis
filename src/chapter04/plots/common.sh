efbccdb72eafd9ba32a60289528c15afff1762db
Joshua Spence <josh@joshuaspence.com>
2012-09-10 14:27:02 +1000

    Improvements to makefile.

--- src/chapter04/plots/common.sh
+++ src/chapter04/plots/common.sh
@@ -27,10 +27,10 @@ BLOCKSIZE_NAMES=( $(cat $DATA_FILE | $UNIQUE_FROM_CSV $(expr $COL_BLOCKSIZE - 1)
 BLOCKSIZE_COLOURS=( $(for i in $(seq 0 ${#BLOCKSIZE_NAMES[*]}); do echo "$(expr 1 + $i)"; done) )
 ALL_BLOCKSIZES=( $(seq 0 $(expr ${#BLOCKSIZE_NAMES[*]} - 1) ) )
 
-GNUPLOT_HEADER=<<END_OF_GNUPLOT
+GNUPLOT_HEADER="
 reset
 set terminal tikz solid color size 10cm, 20.88cm
-set datafile separator ","
+set datafile separator ','
 
 # Define axis
 # Remove border on top and right and set color to gray
@@ -40,4 +40,4 @@ set border 3 back ls 11
 # Style definitions
 set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
 set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2 # --- green
-END_OF_GNUPLOT
+"
