d67971cdcaf1c6d4e4b21d420217982dd9c27f5a
Joshua Spence <josh@joshuaspence.com>
2012-09-14 14:38:22 +1000

    Using pgf-pie instead of gnuplot (with tikz terminal) for creating pie plots.

--- src/chapter04/plots/matlab_profiling/common.pl
+++ src/chapter04/plots/matlab_profiling/common.pl
@@ -34,6 +34,13 @@ use constant COL_SELFTIMEREL  => 9;
 scalar(@ARGV) >= 1 || die("No output file specified!\n");
 my $output_file = $ARGV[0];
 
+sub clean($) {
+    my $cleaned = $_[0];
+    $cleaned =~ s/_/\\_/g;
+    $cleaned =~ s/\.\.\./\\ldots{}/g;
+    return $cleaned;
+}
+
 # Parse the data
 my %data = ();
 open(FILE, "< $DATA_FILE") || die("Cannot open file: $DATA_FILE");
@@ -80,92 +87,32 @@ for (<FILE>) {
 }
 close(FILE);
 
-if (basename($0) =~ m/all_datasets.tex.pl/) {
-    open(TEX, ">$output_file") or die("Cannot open file: $output_file");
-    
-    for my $dataset (keys %data) {
-        print TEX <<END_OF_TEX;
-\\begin{figure}
+open(TEX, ">$output_file") or die("Cannot open file: $output_file");
+
+for my $dataset (keys %data) {
+    print TEX <<END_OF_TEX;
+\\begin{tikzpicture}
 	\\centering
-	\\input{chapter04/plots/matlab_profiling/$dataset}
-	\\caption{}
-	\\label{fig:matlabProfiling:$dataset}
-\\end{figure}
-
+	\\pie[text=legend]{
 END_OF_TEX
-    }
-    
-    close(TEX);
-} else {
-    open(GNUPLOT,"|gnuplot");
-    print GNUPLOT <<END_OF_GNUPLOT;
-reset
-set terminal tikz solid color size 10cm, 20.88cm
-set datafile separator ','
-
-# Global options
-set size square
-set isosample 50,50
-set parametric
-set xrange [-1:1]
-set yrange [-1:1]
-set vrange [0:1]
-unset border
-unset xtics
-unset ytics
-unset colorbox
-
-set view map
-#set palette rgbformulae 7,5,15
-set palette defined (0 0 0 0, 1 0 0 1, 3 0 1 0, 4 1 0 0, 6 1 1 1)
-set cbrange [0:50] # the color palette
-
-END_OF_GNUPLOT
-
-    my $u_begin = 0;
-    my $u_end = 0;
-    my $i = 0;
-    my $dataset = basename($0, ".tex.pl");
-    
-    print GNUPLOT <<END_OF_GNUPLOT;
-################################################################################
-# $dataset
-################################################################################
-set output "$output_file"
-set multiplot
-
-END_OF_GNUPLOT
     
     my $the_profile = PROFILE;
+    my @output = ();
     for my $function (keys %{$data{$dataset}{$the_profile}}) {
-        my $proportion = $data{$dataset}{$the_profile}{$function};
-        
-        if ($proportion > 0) {
-            $u_end = $u_begin + $proportion;
-            my $angle = ($u_begin + $u_end) * pi;
-            
-            my $x = cos($angle);
-            $x = ($x > 0 && $x + 0.2 || $x - 0.2);
-            my $y = sin($angle);
-            $y = ($y > 0 && $y + 0.2 || $y - 0.2);
-            
-            my $i_plus_one = $i + 1;
-            
-            print GNUPLOT <<END_OF_GNUPLOT;
-set urange [$u_begin*2*pi : $u_end*2*pi]
-splot cos(u)*v,sin(u)*v,$i w pm3d notitle
-#set label $i_plus_one center "$function" at $x,$y rotate by $angle*180/pi
-END_OF_GNUPLOT
-            
-            $u_begin = $u_end;
-            $i++;
-        }
+        my $function_clean = clean($function);
+        my $proportion = 100 * $data{$dataset}{$the_profile}{$function};
+        push(@output, "$proportion/{$function_clean}");
     }
+    print TEX join(",\n", @output);
     
-    print GNUPLOT <<END_OF_GNUPLOT;
-unset multiplot
+    print TEX <<END_OF_TEX;
+    
+	}
+	%\\caption{}
+	\\label{fig:matlabProfiling:$dataset}
+\\end{tikzpicture}
 
-END_OF_GNUPLOT
-
-    close(GNUPLOT);
+END_OF_TEX
 }
+
+close(TEX);
