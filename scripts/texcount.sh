48ea56f5a6c0394064484e4e0c5deee9d4484815
Joshua Spence <josh@joshuaspence.com>
2012-10-08 01:33:14 +1100

    Minor improvements.

--- scripts/texcount.sh
+++ scripts/texcount.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+TEXCOUNT="$(dirname $0)/../ext/texcount/texcount.pl"
 SRC_DIR="$(dirname $0)/../src"
-$(dirname $0)/texcount/texcount.pl -col -merge -incbib -total -dir=$SRC_DIR/ $SRC_DIR/thesis.tex
+if [ -x $TEXCOUNT ]; then
+    $TEXCOUNT -col -merge -incbib -total -dir=$SRC_DIR/ $SRC_DIR/thesis.tex
+else
+    echo "Executable not found: $TEXCOUNT" >&2
+    exit 1
+fi
--- scripts/wordcount.sh
+++ scripts/wordcount.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-OUTPUT_PDF=$(readlink -f $(dirname $0))/../src/thesis.pdf
-pdftotext $OUTPUT_PDF - | egrep -e '\w\w\w+' | iconv -f ISO-8859-15 -t UTF-8 | wc -w
+OUTPUT_PDF="$(dirname $0)/../src/thesis.pdf"
+if [ -f $OUTPUT_PDF ]; then
+    pdftotext $OUTPUT_PDF - | egrep -e '\w\w\w+' | iconv -f ISO-8859-15 -t UTF-8 | wc -w
+else
+    echo "File not found: $OUTPUT_PDF" >&2
+    exit 1
+fi
