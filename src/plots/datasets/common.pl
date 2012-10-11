2d86058b2b7e7543e1c21f18754f76596aa58d4d
Joshua Spence <josh@joshuaspence.com>
2012-10-11 22:33:48 +1100

    Added scripts to plot datasets from CSV files using matlab and matlab2tikz

---
+++ src/plots/datasets/common.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Cwd 'abs_path';
+use File::Basename;
+use File::Spec::Functions qw(catdir catfile devnull);
+
+#===============================================================================
+# Configuration
+#===============================================================================
+use constant DATA_DIR => 'data';
+use constant DATA_EXT => '.csv';
+use constant MATLAB_TO_TIKZ_DIR => 'matlab2tikz';
+#-------------------------------------------------------------------------------
+
+# Make sure an output file was specified
+scalar(@ARGV) >= 1 || die('No output file specified!');
+my $output_file = $ARGV[0];
+
+# The data file
+my $data_file = catfile(dirname($0), DATA_DIR, basename($0, ".tex.pl") . DATA_EXT);
+(-f $data_file) || die("Data file not found: $data_file");
+
+# Create the graphs
+open(MATLAB, "|matlab -nosplash -nodisplay >${\(devnull())}");
+print MATLAB <<END_OF_MATLAB;
+addpath('${\(catfile(dirname($0), MATLAB_TO_TIKZ_DIR))}');
+
+in_file = '$data_file';
+out_file = '$output_file';
+
+X = csvread(in_file);
+n = size(X, 1);
+d = size(X, 2);
+
+if d == 2 || d == 3
+    if d == 2
+        h = scatter(X(:,1), X(:,2), 'x');
+    elseif d == 3
+        h = scatter3(X(:,1), X(:,2), X(:,3), 'x');
+    end
+    matlab2tikz(out_file, 'height', '\\figureheight', 'width', '\\figurewidth', 'showInfo', false);
+else
+    % Touch an empty file
+    fclose(fopen(out_file, 'w'));
+end
+END_OF_MATLAB
+
+# Clean up
+close(MATLAB);
